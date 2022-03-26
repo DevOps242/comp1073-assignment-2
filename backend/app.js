51506fe1e70231669810508b00c422d2da892deb
Akeem Palmer <akeemk.palmer@gmail.com>
2022-03-25 22:38:55 -0400

    Update: added the skeleton for the pages and includes the navbar and page routes

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,53 @@
-
-// require the express frame work 
-const express = require('express');
-
-// require the dotenv to load config variables
-const dotenv = require('dotenv');
+const express = require('express');                                                                                               // require the express frame work 
+const dotenv = require('dotenv');                                                                                                 // require the dotenv to load config variables
 
 // Load config
 dotenv.config({path: './config/config.env'});
 
-// import the mongo database connection 
-const connectDB = require('./Database/db.js');
+const connectDB = require('./Database/db.js');                                                                                     // import the mongo database connection 
+connectDB();                                                                                                                       // call the Db connection
 
-// call the Db connection 
-connectDB();
+const app = express();                                                                                                             // call the express frame work object on the app const.
 
-// call the express frame work object on the app const.
-const app = express();
+const HTML_PATH = __dirname + '/views/pages/';
 
-// set the port number from the config variable 
-const PORT = process.env.PORT || 3000;
+app.use(express.static(HTML_PATH));
+app.use(express.static(__dirname + '/views/'));
+// console.log(__dirname);   
 
-// set the server listener to listen on port variable.
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const path = require('path');                                                                                                       // Require the system path 
+     
+// @ routes Adding the routes for the Express frame work to serve.
+// Index Page Endpoint
+app.get('/index', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'index.html'));
+});
+
+// Customers Page Endpoint
+app.get('/customers', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'customers.html'));
+});
+
+// Employees Page Endpoint
+app.get('/employees', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'employees.html'));
+});
+
+// Inventory Page Endpoint
+app.get('/inventory', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'inventory.html'));
+});
+
+// Products Page Endpoint
+app.get('/products', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'products.html'));
+});
+
+// Suppliers Page Endpoint
+app.get('/suppliers', function(req, res) {
+    res.sendFile(path.join(HTML_PATH + 'suppliers.html'));
+});
 
 
-
-
+const PORT = process.env.PORT || 3000;                                                                                             // set the port number from the config variable 
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));                                   // set the server listener to listen on port variable.
